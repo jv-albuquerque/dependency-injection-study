2c837267cd6eca341377845085a006be88d669b6
João Albuquerque <jv.albuquerque@live.com>
2023-01-29 21:45:30 -0300

    Ship movement

--- Assets/_Project/_Scripts/Player/ShipMovement.cs
+++ Assets/_Project/_Scripts/Player/ShipMovement.cs
@@ -6,11 +6,28 @@ namespace Major.SpaceInvaders.Player
 {
     public class ShipMovement : MonoBehaviour
     {
+        [SerializeField] private float _acceleretion = 10.0f;
+        [SerializeField] private float _maxSpeed = 1.0f;
+
         private Vector2 _moveDirection = Vector2.zero;
 
-        private void Update()
-        {
+        private Rigidbody2D rb;
 
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            AddVelocity();
+        }
+
+        private void AddVelocity()
+        {
+            var currentSpeed = _acceleretion * _maxSpeed * Time.fixedDeltaTime;
+            currentSpeed = Mathf.Clamp(currentSpeed, -_maxSpeed, _maxSpeed);
+            rb.velocity = _moveDirection * currentSpeed;
         }
 
         public void SetMoveDirection(Vector2 dir)
